665b7d57f81216d136bfa69fa5b4da33f28ef19b
PengFei <626138959@qq.com>
2018-01-13 16:18:04 +0800

    后台管理整体布局结构

--- src/component/admin/Admin.vue
+++ src/component/admin/Admin.vue
@@ -1,8 +1,22 @@
 <template>
-    <div>
-        <Aside></Aside>
-        <Header></Header>
-    </div>
+    <el-container class="admin">
+        <!-- 页面左侧 -->
+        <el-aside width="200px">
+            <Aside></Aside>
+        </el-aside>
+
+        <!-- 页面右侧 -->
+        <el-container>
+            <!-- 右侧头部 -->
+            <el-header>
+                <Header></Header>
+            </el-header>
+            <!-- 右侧主体, 将来通过路由控制 -->
+            <el-main>
+                <router-view></router-view>
+            </el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -20,5 +34,22 @@
 </script>
 
 <style scoped>
+    .el-header {
+        background-color: #B3C0D1;
+        color: #333;
+    }
+  
+    .el-aside {
+        background-color: #D3DCE6;
+        color: #333;
+    }
 
+    .el-main {
+        background-color: #E9EEF3;
+        color: #333;
+    }
+
+    .admin {
+        height: 100%;
+    }
 </style>
